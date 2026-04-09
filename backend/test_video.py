c8954ef8c4171f18c0f9c7cfef40f509a6c84390
krishsharmacse <godofwarghostofsparta159@gmail.com>
2026-04-09 16:36:51 +0530

    commit

--- backend/test_video.py
+++
@@ -1,9 +0,0 @@
-import requests
-import io
-
-response = requests.post(
-    "http://127.0.0.1:8000/api/video",
-    files={"file": ("dummy.mp4", b"dummy content", "video/mp4")}
-)
-print(response.status_code)
-print(response.json())
